12e24e72fa1edf6439692c714fce689998796473
SamsterBirdies <71206056+SamsterBirdies@users.noreply.github.com>
2021-12-01 19:16:42 -0800

    Added new backgrounds.
    Including custom gradient backgrounds.

--- scripts/cookieAPI.js
+++ scripts/cookieAPI.js
@@ -14,22 +14,39 @@ function cookieRead(k){
 	return 'nocookie';
 }
 //function for changing style option
-function cookieChangeStyle(){
+function cookieChangeStyle(globalpath = false){
 	let style = cookieRead('style');
 	//start changing stuff
 	function setBG(type){
 		if (type != 'none'){
-			document.body.style.backgroundImage = 'url(./images/'+type+'.webp)';
+			if (globalpath){
+				document.body.style.backgroundImage = 'url(https://www.samsterbirdies.com/images/'+type+'.webp)';
+			} else {
+				document.body.style.backgroundImage = 'url(./images/'+type+'.webp)';
+			}
 		} else {
 			document.body.style.backgroundImage = 'none';
 			document.body.style.backgroundColor='#252525';
 		}
-			document.body.style.backgroundRepeat= 'no-repeat';
-			document.body.style.backgroundAttachment= 'fixed';
-			document.body.style.backgroundSize= 'cover';
+		document.body.style.backgroundRepeat= 'no-repeat';
+		document.body.style.backgroundAttachment= 'fixed';
+		document.body.style.backgroundSize= 'cover';
+	}
+	function gradient(){
+		let gd = cookieRead('gradient').split('^^^');
+		document.body.style.backgroundImage = 'linear-gradient('+gd[0]+'deg, rgba('+gd[1]+','+gd[2]+','+gd[3]+',1), rgba('+gd[4]+','+gd[5]+','+gd[6]+',1))';
+		document.body.style.backgroundRepeat= 'no-repeat';
+		document.body.style.backgroundAttachment= 'fixed';
+		document.body.style.backgroundSize= 'cover';
 	}
 	if (style == 'background1'){
 		setBG('background1');
+	} else if (style == 'space'){
+		setBG('backgroundspace');
+	} else if (style == 'schematic') {
+		setBG('backgroundschematic');
+	} else if (style == 'gradient') {
+		gradient();
 	} else {
 		setBG('none');
 	}
